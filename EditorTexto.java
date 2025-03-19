6342c0be464d05ba38d2508388c4018e99cd1949
Fernando <f95saenz@gmail.com>
2025-03-18 22:18:26 -0600

    first commit

--- EditorTexto.java
+++ EditorTexto.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+import java.util.Stack;
 
+public class EditorTexto {
+    private Stack<String> texto;
+    private Stack<String> deshechos;
+
+
+    public EditorTexto(){
+        this.texto = new Stack<>();
+        this.deshechos = new Stack<>();
+    }
+
+    public void agregarLinea(String linea) {
+        texto.push(linea);
+        deshechos.clear();
+    }
+
+    public void deshacer(){
+        if (!texto.isEmpty()) {
+            deshechos.push(texto.pop());
+        }
+    }
+    
+}
